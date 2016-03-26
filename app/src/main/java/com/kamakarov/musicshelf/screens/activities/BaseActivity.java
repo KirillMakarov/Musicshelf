661fd85424399fe7f09cd1643114f61e5f0a5b2e
Kirill Makarov <kir-maka@yandex.ru>
2016-03-27 01:44:15 +0300

    add retrolambda + butterknife to project

--- app/src/main/java/com/kamakarov/musicshelf/screens/activities/BaseActivity.java
+++ app/src/main/java/com/kamakarov/musicshelf/screens/activities/BaseActivity.java
@@ -2,5 +2,12 @@ package com.kamakarov.musicshelf.screens.activities;
 
 import android.support.v7.app.AppCompatActivity;
 
-public abstract class BaseActivity  extends AppCompatActivity{
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
 }
--- app/src/main/java/com/kamakarov/musicshelf/screens/activities/MainActivity.java
+++ app/src/main/java/com/kamakarov/musicshelf/screens/activities/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import com.kamakarov.musicshelf.R;
 
-public final class MainActivity extends BaseActivity {
+import butterknife.ButterKnife;
 
+public final class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
