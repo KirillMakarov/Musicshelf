cc11b79e522e1191c6f76a1d02ccd822c6cc361d
Kirill Makarov <kir-maka@yandex.ru>
2016-03-27 02:06:20 +0300

    add rxandroid

--- app/src/main/java/com/kamakarov/musicshelf/screens/activities/BaseActivity.java
+++ app/src/main/java/com/kamakarov/musicshelf/screens/activities/BaseActivity.java
@@ -1,13 +1,25 @@
 package com.kamakarov.musicshelf.screens.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
+import rx.subscriptions.CompositeSubscription;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    protected CompositeSubscription compositeSubscription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        compositeSubscription = new CompositeSubscription();
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        compositeSubscription.unsubscribe();
         ButterKnife.unbind(this);
     }
 }
