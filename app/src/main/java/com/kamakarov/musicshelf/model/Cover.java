a0de16da325cbee7f819a1cb2ce81467be6aa23b
Kirill Makarov <kir-maka@yandex.ru>
2016-04-19 11:54:35 +0300

    make cover and singer parcelable

--- app/src/main/java/com/kamakarov/musicshelf/model/Cover.java
+++ app/src/main/java/com/kamakarov/musicshelf/model/Cover.java
@@ -1,6 +1,9 @@
 package com.kamakarov.musicshelf.model;
 
-public final class Cover {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public final class Cover implements Parcelable {
     private String small;
     private String big;
 
@@ -11,4 +14,36 @@ public final class Cover {
     public String getSmall() {
         return small;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.small);
+        dest.writeString(this.big);
+    }
+
+    public Cover() {
+    }
+
+    protected Cover(Parcel in) {
+        this.small = in.readString();
+        this.big = in.readString();
+    }
+
+    public static final Parcelable.Creator<Cover> CREATOR = new Parcelable.Creator<Cover>() {
+        @Override
+        public Cover createFromParcel(Parcel source) {
+            return new Cover(source);
+        }
+
+        @Override
+        public Cover[] newArray(int size) {
+            return new Cover[size];
+        }
+    };
 }
--- app/src/main/java/com/kamakarov/musicshelf/model/Singer.java
+++ app/src/main/java/com/kamakarov/musicshelf/model/Singer.java
@@ -1,8 +1,11 @@
 package com.kamakarov.musicshelf.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public final class Singer {
+public final class Singer implements Parcelable {
     private long id;
     private String name;
     private List<String> genres;
@@ -43,4 +46,48 @@ public final class Singer {
     public Cover getCover() {
         return cover;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.id);
+        dest.writeString(this.name);
+        dest.writeStringList(this.genres);
+        dest.writeInt(this.tracks);
+        dest.writeInt(this.albums);
+        dest.writeString(this.link);
+        dest.writeString(this.description);
+        dest.writeParcelable(this.cover, flags);
+    }
+
+    public Singer() {
+    }
+
+    protected Singer(Parcel in) {
+        this.id = in.readLong();
+        this.name = in.readString();
+        this.genres = in.createStringArrayList();
+        this.tracks = in.readInt();
+        this.albums = in.readInt();
+        this.link = in.readString();
+        this.description = in.readString();
+        this.cover = in.readParcelable(Cover.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Singer> CREATOR = new Parcelable.Creator<Singer>() {
+        @Override
+        public Singer createFromParcel(Parcel source) {
+            return new Singer(source);
+        }
+
+        @Override
+        public Singer[] newArray(int size) {
+            return new Singer[size];
+        }
+    };
 }
