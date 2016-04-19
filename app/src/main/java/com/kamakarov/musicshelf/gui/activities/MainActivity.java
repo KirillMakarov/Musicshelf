9d1e20e0bacf76f6a8ce3d909d484c8eb52d7257
Kirill Makarov <kir-maka@yandex.ru>
2016-04-19 11:06:09 +0300

    change name of app in shortcuts

--- app/src/main/java/com/kamakarov/musicshelf/gui/activities/MainActivity.java
+++ app/src/main/java/com/kamakarov/musicshelf/gui/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kamakarov.musicshelf.gui.activities;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.kamakarov.musicshelf.R;
 import com.kamakarov.musicshelf.gui.fragments.SingerListFragment;
 
 public final class MainActivity extends SingleFragmentActivity {
@@ -9,5 +11,11 @@ public final class MainActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return SingerListFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.artists_label);
     }
 }
