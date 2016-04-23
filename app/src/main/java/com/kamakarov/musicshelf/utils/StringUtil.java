1e0c2443d62c1bd3298678a9010d6a1c0637924c
Kirill Makarov <kir-maka@yandex.ru>
2016-04-23 12:41:34 +0300

    add jetbrains annotations to project

--- app/src/main/java/com/kamakarov/musicshelf/utils/StringUtil.java
+++ app/src/main/java/com/kamakarov/musicshelf/utils/StringUtil.java
@@ -1,9 +1,12 @@
 package com.kamakarov.musicshelf.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class StringUtil {
-    public static String concatenateWithComma(List<String> stringList) {
+
+    public static String concatenateWithComma(@NotNull List<String> stringList) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < stringList.size(); i++) {
             sb.append(stringList.get(i));
